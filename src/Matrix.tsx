42ce28f8ea1b12dd48dea1c6b4adbf659906851b
Ramon Ankersmit <ramon_ankersmit@hotmail.com>
2025-09-18 22:20:14 +0200

    Fix: Remove duplicate '+ Eigen huiswerk toevoegen' button in Matrix.tsx

---
+++ src/Matrix.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Matrix = () => {
+    return (
+        <div>
+            {/* Other components */}
+            {/* Ensure only one 'Add homework' button is present */}
+            <button onClick={handleAddHomework}>+ Eigen huiswerk toevoegen</button>
+        </div>
+    );
+};
+
+const handleAddHomework = () => {
+    // Logic to add homework
+};
+
+export default Matrix;
